1bb2e2029c7964f3895e5262da26610ae0d402b8
Даниил Чемеркин <daminik00@MacBook-Pro-Daniil.local>
2017-05-27 14:41:37 +0300

    Error Fix

--- parseRoutes.php
+++ parseRoutes.php
@@ -1,6 +1,18 @@
 <?php
 
-  $url = 'https://www.its-rnd.ru/pikasonline/rostov/routes.txt?1495875600000';
+  $url = 'https://www.its-rnd.ru/pikasonline/rostov/routes.txt';
   $txt = file_get_contents($url);
-  echo $txt;
+  // echo $txt;
+
+  $handle = fopen($url, "r");
+if ($handle) {
+    while (($line = fgets($handle)) !== false) {
+        // process the line read.
+        echo $line;
+    }
+
+    fclose($handle);
+} else {
+    // error opening the file.
+}
 ?>
