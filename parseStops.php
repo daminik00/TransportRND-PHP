98d8ffaf7fc961f73f776e0bff4b92b19ed3d99b
Даниил Чемеркин <daminik00@MacBook-Pro-Daniil.local>
2017-05-28 19:54:52 +0300

    Dev

--- parseStops.php
+++ parseStops.php
@@ -12,7 +12,20 @@
 
   $array1 = array();
   for ($i = 0; $i < count($array); $i++) {
-    $array1[] = split(';', $array[$i]);
+    $array = split(';', $array[$i]);
+    $array1[] = array(
+      "id" => $array[0],
+      "Lat" => $array[1],
+      "Lng" => $array[2],
+      "Stops" => $array[3],
+      "Name" => $array[4],
+      "Info" => $array[5],
+      "Street" => $array[6],
+      "Area" => $array[7],
+      "City" => $array[8],
+    );
+
+    print_r($array1[$i]);
+    echo "<br /><br />";
   }
-  print_r($array1);
 ?>
